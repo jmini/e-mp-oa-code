4709d1edbd78b4148771179dd64ef81faaece56d
Jeremie Bresson <dev@jmini.fr>
2018-12-28 08:52:48 +0100

    Add 'testRequired'

--- empoa-extended-tck/src/main/java/fr/jmini/empoa/extended/tck/models/media/SchemaTest.java
+++ empoa-extended-tck/src/main/java/fr/jmini/empoa/extended/tck/models/media/SchemaTest.java
@@ -1,6 +1,9 @@
 package fr.jmini.empoa.extended.tck.models.media;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 import org.eclipse.microprofile.openapi.OASFactory;
 import org.eclipse.microprofile.openapi.models.media.Schema;
@@ -14,4 +17,20 @@ public class SchemaTest {
         assertThat(schema).isNotNull();
     }
 
+    @Test
+    public void testRequired() {
+        Schema schema = OASFactory.createObject(Schema.class);
+
+        schema.setRequired(null);
+        assertThat(schema.getRequired()).isNull();
+
+        schema.setRequired(Collections.emptyList());
+        assertThat(schema.getRequired()).isEmpty();
+
+        schema.setRequired(Collections.singletonList("element"));
+        assertThat(schema.getRequired()).containsExactly("element");
+
+        schema.setRequired(Arrays.asList("lorem", "ipsum"));
+        assertThat(schema.getRequired()).containsExactlyInAnyOrder("lorem", "ipsum");
+    }
 }
